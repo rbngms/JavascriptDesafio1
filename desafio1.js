54888804e85a67e150fa3e2f3d39a65e89775be5
rgaraym <rbngms@gmail.com>
2023-03-26 14:37:18 -0500

    desafio1JS

---
+++ desafio1.js
@@ -0,0 +1,40 @@
+
+
+
+/* TEXTO 1 */
+/*
+let total = 0;
+
+while ( total <=50 ) {
+
+    let numero_desafio = parseInt(prompt ("Ingrese un numero para sumar a total"));
+    let numero_entero = parseInt (numero_desafio);
+
+    total = total + numero_desafio;
+
+    console.log("el valor de total es", total)
+}
+*/
+
+/* TEXTO 2 */
+/*
+let nombre_desafio;
+
+do {
+    nombre_desafio = prompt ("Ingrese su nombre");
+    console.log ("Bievenido/a", nombre_desafio)
+
+}
+
+
+while ( nombre_desafio != "ESC")
+*/
+/* TEXTO 3 */
+
+
+numero_desafio = prompt ("Ingresa un numero")
+for (let i = 0; i < numero_desafio; i++) {
+    // Se ejecuta 5 veces, con valores del paso 0 al 4.
+    
+    console.log("Hola");
+  }
